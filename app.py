87c0cd1a7038a2aeafbc28d04c64bf2f06e9f0e6
MARTIBS <brian.martin@abbvie.com>
2020-08-01 23:57:43 -0500

    Added TODO comments for next steps toward completion

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, render_template
 import requests
 from datetime import datetime, timedelta
 
-now = datetime.now()
-
 app = Flask(__name__)
 
+# TODO move Snapshot classes to a python package
+
+# TODO add class documentation to District class
 class District:
+    # TODO move hard coded lookup table to a database
     lookup = {
         '115':
             {
@@ -40,6 +42,7 @@ class District:
         self.COVIDRegion = District.lookup[district]['COVIDRegion']
         self.counties = District.lookup[district]['counties']
 
+# TODO add class documentation to SnapshotSummary class
 class SnapshotSummary:
 
     def __init__(self, district):
@@ -53,25 +56,30 @@ class SnapshotSummary:
                               self.staff.status and \
                               self.trans.status and \
                               self.geo.status
-
+# TODO add class documentation to Snapshot class
 class Snapshot:
 
     def __init__(self):
         self.status = 0
 
+# TODO implement PPESnapshot class
 class PPESnapshot(Snapshot):
     pass
 
+# TODO implement StaffSnapshot class
 class StaffSnapshot(Snapshot):
     pass
 
+# TODO implement TransSnapshot class
 class TransSnapshot(Snapshot):
     pass
 
+# TODO move to a utility function library
 def date_in_window(dv, td):
     d = datetime.strptime(dv, '%m/%d/%Y')
     return (d <= datetime.today()) and (d >= (datetime.today()-td))
 
+# TODO replace county_historical by moving into risk_metrics
 def county_historical(district):
     r = requests.get('https://www.dph.illinois.gov/sitefiles/COVIDHistoricalTestResults.json?nocache=1')
     county_history = r.json()['historical_county']
@@ -85,6 +93,7 @@ def county_historical(district):
         })
     print(data)
 
+# TODO move risk_metrics to static object methods
 def risk_metrics(district):
     r = requests.get('https://www.dph.illinois.gov/sitefiles/COVIDCountyRiskMetrics.json?nocache=1')
     tp = r.json()['testPositivity']
@@ -98,23 +107,25 @@ def risk_metrics(district):
         })
     print(data)
 
-
+# TODO add GeoSnapshot classs documentation
 class GeoSnapshot(Snapshot):
 
     def __init__(self, district):
         self.district = district
         risk_metrics(self.district)
+        self.status = 0
 
     def zip_positivity_rate(self):
+        # TODO implement zip_code positivity rate calculation
         pass
 
     def county_positivity_rate(self):
+        # TODO implement county positivity rate calculation
         pass
 
     def region_positivity_rate(self):
+        # TODO implement region positivity rate calculation
         pass
-
-    pass
 
 @app.route('/status')
 def status():
@@ -125,6 +136,8 @@ def get_overall_snapshot(district_id):
     snap = SnapshotSummary(District(district_id))
     return render_template('overall_snapshot.html', snap=snap)
 
+# TODO consolidate get_<type>_snapshot methods using path parameter
+# TODO implement SnapshotFactory class
 @app.route('/snapshot/ppe/<district_id>')
 def get_ppe_snapshot(district_id):
     snap = PPESnapshot(District(district_id))
@@ -145,6 +158,9 @@ def get_geo_snapshot(district_id):
     snap = GeoSnapshot(District(district_id))
     return render_template('geo_snapshot.html', snap)
 
+# TODO create flask methods to support PPE inventory functions
+# TODO create flask methods to support staff inventory functions
+# TODO create flask methods to generate trend analysis views
 
 if __name__ == '__main__':
     app.run()
